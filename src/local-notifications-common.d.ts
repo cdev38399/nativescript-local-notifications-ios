cf924a639ff689b13169759888b5544cd7a763ca
Rudiyanto <rudiyanto2003@gmail.com>
2020-07-29 00:26:28 +0700

    update

--- src/local-notifications-common.d.ts
+++ src/local-notifications-common.d.ts
@@ -2,6 +2,7 @@ import { Color } from "tns-core-modules/color/color";
 export declare type ScheduleInterval = "second" | "minute" | "hour" | "day" | "week" | "month" | "quarter" | "year";
 export interface NotificationAction {
     id: string;
+    id_str: string;
     type: "button" | "input";
     title?: string;
     launch?: boolean;
@@ -38,6 +39,7 @@ export interface ScheduleOptions {
 }
 export interface ReceivedNotification {
     id: number;
+    id_str: string;
     foreground: boolean;
     title?: string;
     body?: string;
